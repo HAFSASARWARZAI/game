93a3615748212cf2c944c96edb2a4d13cf4b3ad3
HAFSASARWARZAI <114585456+HAFSASARWARZAI@users.noreply.github.com>
2023-02-09 09:00:06 -0500

    Add files via upload

---
+++ pygame/click_game.py
@@ -0,0 +1,37 @@
+character = Actor('character-1')
+character .pos = 100,56
+
+WIDTH = 500
+HEIGHT = character .height + 20
+
+def draw():
+    screen.clear()
+    character.draw()
+
+def update():
+    character.left = character.left + 2
+    if character.left > WIDTH:
+        character.right = 0
+
+def on_mouse_down(pos):
+    if character.collidepoint(pos):
+        print("Eek!")
+    else:
+        print("You missed me!")
+
+
+def on_mouse_down(pos):
+    if character.collidepoint(pos):
+        sounds.eep.play()
+        character.image = 'character_hurt'
+
+def on_mouse_down(pos):
+    if character.collidepoint(pos):
+        sounds.eep.play()
+        character.image = 'alien_hurt'
+        time.sleep(1)
+        character.image = 'alien'
+
+
+
+
